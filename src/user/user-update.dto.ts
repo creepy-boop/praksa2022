7600c2a0089ee9d47718386a4c981aab53b9fd07
creepy-boop <77396186+creepy-boop@users.noreply.github.com>
2022-04-19 12:41:25 +0200

    user crud koncan

---
+++ src/user/user-update.dto.ts
@@ -0,0 +1,14 @@
+import {IsEmail, IsNotEmpty, MinLength} from "class-validator";
+
+
+
+export  class UserUpdateDto{
+
+    first_name?: string;
+
+    last_name?: string;
+
+    email?: string;
+
+    password?: string;
+}
